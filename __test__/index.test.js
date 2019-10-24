868e20836e8941695776c7294a105f3c50ab7a8c
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2019-10-24 12:56:42 +0100

    add tests

---
+++ __test__/index.test.js
@@ -0,0 +1,78 @@
+/* eslint-env jest */
+
+const { matchers } = require('jest-json-schema')
+expect.extend(matchers)
+
+const traffic = require('../')
+
+const schema = {
+  type: 'array',
+  items: {
+    type: 'object',
+    required: [
+      'title',
+      'category',
+      'delay',
+      'link',
+      'location'
+    ],
+    properties: {
+      title: {
+        type: 'string'
+      },
+      category: {
+        type: 'string'
+      },
+      delay: {
+        type: 'string'
+      },
+      link: {
+        type: 'string'
+      },
+      location: {
+        type: 'object',
+        required: [
+          'road',
+          'county',
+          'region',
+          'latitude',
+          'longitude'
+        ],
+        properties: {
+          road: {
+            type: 'string'
+          },
+          county: {
+            type: 'string'
+          },
+          region: {
+            type: 'string'
+          },
+          latitude: {
+            type: 'number'
+          },
+          longitude: {
+            type: 'number'
+          }
+        }
+      }
+    }
+  }
+}
+
+describe('end-to-end', () => {
+  describe('incidents', () => {
+    test('it matches the schema', async () => {
+      const result = await traffic.incidents('NORTH_WEST')
+
+      expect(result).toMatchSchema(schema)
+    })
+  })
+  describe('roadworks', () => {
+    test('it matches the schema', async () => {
+      const result = await traffic.roadworks('NORTH_WEST')
+
+      expect(result).toMatchSchema(schema)
+    })
+  })
+})
---
+++ __test__/spec/common/urls.test.js
@@ -0,0 +1,30 @@
+/* eslint-env jest */
+
+const urls = require('../../../src/common/urls')
+
+describe('URLs', () => {
+  describe('incidents', () => {
+    test('defaults to all regions if one is not provided', () => {
+      const url = urls.incidents()
+
+      expect(url).toEqual('https://m.highwaysengland.co.uk/feeds/rss/UnplannedEvents.xml')
+    })
+    test('allows a region to be specified', () => {
+      const url = urls.incidents('NORTH_WEST')
+
+      expect(url).toEqual('https://m.highwaysengland.co.uk/feeds/rss/UnplannedEvents/North%20West.xml')
+    })
+  })
+  describe('roadworks', () => {
+    test('defaults to all regions if one is not provided', () => {
+      const url = urls.roadworks()
+
+      expect(url).toEqual('https://m.highwaysengland.co.uk/feeds/rss/CurrentAndFutureEvents.xml')
+    })
+    test('allows a region to be specified', () => {
+      const url = urls.roadworks('NORTH_WEST')
+
+      expect(url).toEqual('https://m.highwaysengland.co.uk/feeds/rss/CurrentAndFutureEvents/North%20West.xml')
+    })
+  })
+})
