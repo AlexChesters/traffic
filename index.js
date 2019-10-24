28f94a1351e6a09e4cafc73af721862a932d681a
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2019-10-24 12:30:54 +0100

    fix

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const incidents = require('./src/data/incidents')
 const roadworks = require('./src/data/roadworks')
 
-const regions = require('./src/common/regions')
-
-module.exports = { incidents, roadworks, regions }
+module.exports = { incidents, roadworks }
